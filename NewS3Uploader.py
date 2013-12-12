b77d5232391bdf67670dd4f0bcfc3710f1aa4fcc
Grizzel, Travis <neversummer32@gmail.com>
2013-12-12 11:52:14 -0800

    more debug info

--- NewS3Uploader.py
+++ NewS3Uploader.py
@@ -3,18 +3,18 @@
 
 import os
 import boto.s3
+from boto.s3.connection import Location
 
 conn = boto.connect_s3()
 
 def get_region():
 	# Gives the user a list of the current S3 locations
-	from boto.s3.connection import Location
 	print '\n'.join(i for i in dir(Location) if i[0].isupper())
 
 def new_bucket(bucket_name, loc):
 	# Create a new bucket, in a specific location
-	from boto.s3.connection import Location
-	conn.create_bucket(bucket_name, location='Location.%s' %loc) #this line isn't working
+	conn.create_bucket(bucket_name, location='Location.%s' % loc) #Runtime error: The specified location-constraint is not valid</Message><LocationConstraint>Location.EU
+	#conn.create_bucket('specificbucketnamehere', location=Location.EU)  #This line works as location is handled fine
 
 def upload_data():
 	#upload all files in current working directory to S3 bucket
